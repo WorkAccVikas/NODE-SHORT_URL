36cda5c597eb5decc1b00dcfd7f43f1413d5a939
WorkAccVikas <workaccvikas@gmail.com>
2023-12-01 00:37:06 +0530

    c5

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,18 @@
-const { getUser } = require("../service/auth");
 const { getUser1 } = require("../service/auth1");
 
 async function checkAuth(req, res, next) {
-  const userUid = req.cookies?.uid;
+  // const userUid = req.cookies?.uid;
+  // POINT : Check token in header for client (e.g mobile)
+  console.log("Header = ", req.headers);
+  const userUid = req.headers?.["authorization"];
+  const token = userUid.split("Bearer ")[1];
 
   // POINT : Stateful Authentication
   // const user = getUser(userUid);
   // POINT : Stateless Authentication
-  const user = await getUser1(userUid);
+  // const user = await getUser1(userUid);
+  // POINT : For mobile
+  const user = await getUser1(token);
 
   req.user = user;
 
@@ -15,13 +20,20 @@ async function checkAuth(req, res, next) {
 }
 
 async function restrictToLoggedInUserOnly(req, res, next) {
-  const userUid = req.cookies?.uid;
+  // const userUid = req.cookies?.uid;
+  // POINT : Check token in header for client (e.g mobile)
+  console.log("Header = ", req.headers);
+  const userUid = req.headers?.["authorization"];
+  const token = userUid.split("Bearer ")[1];
+
   if (!userUid) return res.redirect("/login");
 
   // POINT : Stateful Authentication
   // const user = getUser(userUid);
   // POINT : Stateless Authentication
-  const user = await getUser1(userUid);
+  // const user = await getUser1(userUid);
+  // POINT : For mobile
+  const user = await getUser1(token);
 
   if (!user) return res.redirect("/login");
 
